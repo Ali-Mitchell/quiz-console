b9abd1c2e7772902b2d94845ed4185dba9966791
AliMitchell <ali.mitchell.n@gmail.com>
2021-04-11 17:31:18 -0600

    updating html elements for Javascript, style changes

---
+++ assets/java.js
@@ -0,0 +1,13 @@
+var timerEl = document.querySelector("#countdown");
+var questionHeader = document.querySelector("#quiz-header");
+var buttons = document.querySelector("#quiz-body");
+var inputButton = document.querySelector("#input");
+var answer = document.querySelector("#answer");
+
+var questionsArr = [
+    {question: "Sample:",
+    options: ["A","B","C","D"],
+    answer:"4"
+}
+
+]
